1085ef697aed9a6b9f540d626d4b9c4e06567939
leo <leo.prudh@gmail.com>
2019-02-25 13:18:22 +0100

    Form and img rectification

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,10 +8,10 @@ class Header extends React.Component {
         return (
             <header id="header">
                 <div className="inner">
-                    <a href="#" className="image avatar"><img src={avatar} alt="" /></a>
+                    <div className="image avatar"><img src={avatar} alt="" /></div>
                     <h1><strong>Bienvenue, je m'apelle Léo Prud'hom</strong></h1><br />
-                    <h1>Vous recherchez un développeur web?<br/> Vous opérez une transformation digitale?<br />
-                    Vous êtes au bon endroit!</h1>
+                    <h1>Vous recherchez un développeur web ?<br/> Vous opérez une transformation digitale ?<br />
+                    Vous êtes au bon endroit !</h1>
                 </div>
                 <Footer />
             </header>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,19 @@ import Helmet from 'react-helmet'
 import Layout from '../components/layout'
 // import Lightbox from 'react-images'
 import Gallery from '../components/Gallery'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button} from "reactstrap"
 
 import thumb01 from '../assets/images/thumbs/01.png'
 import thumb02 from '../assets/images/thumbs/02.png'
 import thumb03 from '../assets/images/thumbs/03.png'
-import thumb04 from '../assets/images/thumbs/04.jpg'
+import thumb04 from '../assets/images/thumbs/04.png'
 
 
 import full01 from '../assets/images/fulls/01.png'
 import full02 from '../assets/images/fulls/02.png'
 import full03 from '../assets/images/fulls/03.png'
-import full04 from '../assets/images/fulls/04.jpg'
+import full04 from '../assets/images/fulls/04.png'
 
 
 const DEFAULT_IMAGES = [
@@ -123,37 +125,17 @@ class HomeIndex extends React.Component {
                     <section id="three">
                         <h2>Get In Touch</h2>
                         <p>Pour tout renseignement, je vous invite à remplir le formulaire ci-dessous et je reviendrai vers vous dans les plus brefs délais.</p>
-                        <div className="row">
-                            <div className="8u 12u$(small)">
+
                             <form method="POST" action="https://formspree.io/leo.prudh@gmail.com">
-                                <input type="text" name="name" placeholder="Votre nom"/>
-                                <input type="email" name="email" placeholder="Votre email"/>
-                                <textarea name="message" placeholder="Message"></textarea>
-                                <button type="submit">Envoyer</button>
+                                <input type="text" name="name" placeholder="Votre nom" style={{width:'50%', margin:'1%'}}/>
+                                <input type="email" name="email" placeholder="Votre email" style={{width:'50%', margin:'1%'}}/>
+                                <textarea name="message" placeholder="Message" style={{width:'70%', margin:'1%'}}></textarea>
+
+                                    <Button color="primary" style={{margin:'1%'}}>Envoyer</Button>
+
+                                
                             </form>
-                                <ul className="actions">
-                                    <li><input type="submit" value="Send Message" /></li>
-                                </ul>
-                            </div>
-                            <div className="4u 12u$(small)">
-                                <ul className="labeled-icons">
-                                    <li>
-                                        <h3 className="icon fa-home"><span className="label">Address</span></h3>
-                                        1234 Somewhere Rd.<br />
-                                        Nashville, TN 00000<br />
-                                        United States
-                                    </li>
-                                    <li>
-                                        <h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
-                                        000-000-0000
-                                    </li>
-                                    <li>
-                                        <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
-                                        <a href="#">hello@untitled.tld</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+
                     </section>
 
                 </div>
