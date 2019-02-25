b231761b5fe5b440e5777bb32d9d2946320a3027
leo <leo.prudh@gmail.com>
2019-02-25 13:23:56 +0100

    Adding some text

--- src/pages/index.js
+++ src/pages/index.js
@@ -88,10 +88,10 @@ class HomeIndex extends React.Component {
 
                     <section id="intro">
                         <header className="major">
-                            <h2>À propos de Léo<br />
+                            <h2>À propos de Léo,<br />
                             Développeur d'applications web.</h2>
                         </header>
-                        <p>Je suis un jeune Freelance passionné par son job, les sorties en nature, les sports de glisse et le café! <i className="icon fa-coffee"></i> <br/>
+                        <p>Je suis un jeune développeur web en Freelance passionné par son job, les sorties en nature, les sports de glisse et le café! <i className="icon fa-coffee"></i> <br/>
                         De nature optimiste, j'aime relever des défis et faire évoluer les choses en alliant travail et bonne humeur. 
                         Venez avec vos idées, dicutons-en autour d'un café ou organisons un visio-call, et travaillons ensemble pour réaliser des projets à la hauteur de vos ambitions! 
                         </p>
