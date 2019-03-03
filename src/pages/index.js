a5435ad82195a3cdb320a623b07934d9614af3ac
leo <leo.prudh@gmail.com>
2019-03-03 18:07:00 +0100

    social card implementation

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ import full02 from '../assets/images/fulls/02.png'
 import full03 from '../assets/images/fulls/03.png'
 import full04 from '../assets/images/fulls/04.png'
 
-import siteMetaImg from '../../public/static/img/LeoPrudSite.png'
+import siteMetaImg from '../assets/LeoPrudSite.png'
 
 
 const DEFAULT_IMAGES = [
